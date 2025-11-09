d2e6526fa1e6be119f2c2bd1fc728f447aebb5a1
Rakha adi <rakhaadi24@gmail.com>
2025-11-09 12:56:54 +0700

    remove web

--- web/src/components/theme-provider.tsx
+++
@@ -1,6 +0,0 @@
-import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ReactNode } from "react"
-
-export function ThemeProvider({ children, ...props }: { children: ReactNode; [key: string]: any }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
