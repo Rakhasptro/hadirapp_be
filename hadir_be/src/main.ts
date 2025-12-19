f569198719c0000200d3700e0fe2563177a22ce1
rakha <rakhaadi24@gmail.com>
2025-12-19 22:02:35 +0700

    remove swagger

--- hadir_be/src/main.ts
+++ hadir_be/src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
@@ -28,15 +27,7 @@ app.enableCors({
   
   app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
-
-  const config = new DocumentBuilder()
-    .setTitle('HadirApp API')
-    .setDescription('API documentation for attendance system')
-    .setVersion('1.0')
-    .addBearerAuth()
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
+  
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
